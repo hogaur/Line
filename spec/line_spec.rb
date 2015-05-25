9796ce498eba2b372c8cdf861c5e8ed8434fb93e
HariOm Gaur <hogaur@HariOms-MacBook-Pro.local>
2015-05-25 15:03:16 +0530

    [Hari] Adding Gemfile, line_spec

---
+++ spec/line_spec.rb
@@ -0,0 +1,7 @@
+describe 'Line' do
+  it 'should declare a line with start_point and end_point' do
+    line=Line.new [1,1],[2,2]
+    expect(line.start_point).to eq([1,1])
+    expect(line.end_point).to eq([2,2])
+  end
+end
