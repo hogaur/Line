e66427c07f43ed50f1e83f06686b0d058ac2e241
HariOm Gaur <hogaur@HariOms-MacBook-Pro.local>
2015-05-25 15:52:48 +0530

    [Hari] Line::length added

--- lib/line.rb
+++ lib/line.rb
@@ -4,4 +4,9 @@ class Line
     @start_point = start_point
     @end_point = end_point
   end
+  def length
+    dx=(@end_point[0]-@start_point[0]).abs
+    dy=(@end_point[1]-@start_point[1]).abs
+    return Math.sqrt(dx*dx+dy*dy)
+  end
 end
--- spec/line_spec.rb
+++ spec/line_spec.rb
@@ -5,4 +5,8 @@ describe 'Line' do
     expect(line.start_point).to eq([1,1])
     expect(line.end_point).to eq([2,2])
   end
+  it 'should calculate the length of the line formed by start_point and end_point' do
+    line=Line.new [1,1],[7,9]
+    expect(line.length).to eq(10)
+  end
 end
