53fc8ff767e2a3750f5c396124604d5b9c03de82
HariOm Gaur <hogaur@HariOms-MacBook-Pro.local>
2015-05-25 15:29:54 +0530

    [Hari] Line constructor added

--- lib/line.rb
+++ lib/line.rb
@@ -1,5 +1,7 @@
 class Line
-  attr_accessor :start_point :end_point
+  attr_accessor :start_point, :end_point
   def initialize start_point, end_point
+    @start_point = start_point
+    @end_point = end_point
   end
 end
