538020cb47fd0a511953daffcfac4cd881f68a28
HariOm Gaur <hogaur@HariOms-MacBook-Pro.local>
2015-05-26 13:22:36 +0530

    [Hari|Shivam|Guru] Line equality code and spec added

--- lib/line.rb
+++ lib/line.rb
@@ -1,12 +1,17 @@
 class Line
   attr_accessor :start_point, :end_point
+  
   def initialize start_point, end_point
-    @start_point = start_point
-    @end_point = end_point
+    @start_point = Point.new start_point.x, start_point.y
+    @end_point = Point.new end_point.x, end_point.y
   end
+
   def length
-    dx=(@end_point[0]-@start_point[0]).abs
-    dy=(@end_point[1]-@start_point[1]).abs
-    return Math.sqrt(dx*dx+dy*dy)
+    Point.distance_between @start_point, @end_point
   end
+  
+  def Line.equal? line1,line2
+    (Point.equal? line1.start_point, line2.start_point and  Point.equal? line1.end_point, line2.end_point) or
+      (Point.equal? line1.start_point, line2.end_point and  Point.equal? line1.end_point, line2.start_point)
+  end 
 end
--- spec/line_spec.rb
+++ spec/line_spec.rb
@@ -1,12 +1,27 @@
 require 'spec_helper'
 describe 'Line' do
   it 'should declare a line with start_point and end_point' do
-    line=Line.new [1,1],[2,2]
-    expect(line.start_point).to eq([1,1])
-    expect(line.end_point).to eq([2,2])
+    point1 = Point.new 1, 1
+    point2 = Point.new 2, 2
+    line=Line.new point1, point2
+    expect(Point.equal? line.start_point, point1).to eq(true)
+    expect(Point.equal? line.end_point, point2).to eq(true)
   end
-  it 'should calculate the length of the line formed by start_point and end_point' do
-    line=Line.new [1,1],[7,9]
-    expect(line.length).to eq(10)
+  it 'should return length of the line' do
+    point1 = Point.new 1, 1
+    point2 = Point.new 4, 5
+    line=Line.new point1, point2
+    expect(line.length).to eq(5)
+  end
+  describe 'equality' do
+    it 'should return true for equal lines' do
+      point1 = Point.new 1, 1
+      point2 = Point.new 4, 5
+      line1=Line.new point1, point2
+      line2=Line.new point1, point2
+      line3=Line.new point2, point1
+      expect(Line.equal? line1,line2).to eq(true)
+      expect(Line.equal? line1,line3).to eq(true)
+    end
   end
 end
