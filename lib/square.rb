60617137d43c9949cef6e2f897557343074beb76
HariOm Gaur <hogaur@HariOms-MacBook-Pro.local>
2015-05-28 10:38:37 +0530

    [Hari| Shivam| Guru] Square specs and class refactored as per Measure class functionality

--- lib/square.rb
+++ lib/square.rb
@@ -1,11 +1,11 @@
 class Square < Rectangle
-  def self.new_with_sides pivot, side
-    super pivot, side, side
+  def self.new_with_sides side
+    super side, side
   end
 
   def self.valid? point1, point2, point3, point4
     line1 = Line.new point1, point2
     line4 = Line.new point4, point1
-    super and (line1.length == line4.length)
+    super and (Measure.equal? line1.length, line4.length)
   end
 end
--- spec/square_spec.rb
+++ spec/square_spec.rb
@@ -1,16 +1,16 @@
 require 'spec_helper'
 
 describe 'Square' do
-  it 'should declare a square with pivot and one side' do
+  it 'should declare a square with a side' do
     pivot = Point.new 0,0
-    side = 10
-    square = Square.new_with_sides pivot, side
+    side = Measure.new "10 mm"
+    square = Square.new_with_sides side
     expect(Point.equal? square.point1, pivot).to eq(true)
-    expect(Point.equal? square.point2, Point.new(pivot.x + side, pivot.y)).to eq(true)
-    expect(Point.equal? square.point3, Point.new(pivot.x + side, pivot.y + side)).to eq(true)
-    expect(Point.equal? square.point4, Point.new(pivot.x, pivot.y + side)).to eq(true)
+    expect(Point.equal? square.point2, Point.new(pivot.x + side.numeral, pivot.y)).to eq(true)
+    expect(Point.equal? square.point3, Point.new(pivot.x + side.numeral, pivot.y + side.numeral)).to eq(true)
+    expect(Point.equal? square.point4, Point.new(pivot.x, pivot.y + side.numeral)).to eq(true)
   end
-  
+
   it 'should declare a square with four points' do
     point1 = Point.new 0, 0
     point2 = Point.new 2, 0
@@ -39,7 +39,8 @@ describe 'Square' do
       point3 = Point.new 2, 2
       point4 = Point.new 0, 2
       square= Square.new point1, point2, point3, point4
-      expect(square.area).to eq(4)
+      expect(square.area.numeral).to eq(4)
+      expect(square.area.unit).to eq("mm2")
     end
   end
 
@@ -50,7 +51,8 @@ describe 'Square' do
       point3 = Point.new 2, 2
       point4 = Point.new 0, 2
       square= Square.new point1, point2, point3, point4
-      expect(square.perimeter).to eq(8)
+      expect(square.perimeter.numeral).to eq(8)
+      expect(square.perimeter.unit).to eq("mm")
     end
   end
 end
